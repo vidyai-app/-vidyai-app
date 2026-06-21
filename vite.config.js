dd063b96644db90e6e28af5d9ad8a084c212f0a3
vidyai-app <vidyai-app@users.noreply.github.com>
2026-06-21 21:53:38 +0000

    fix: force new bundle hash to bust Vercel cache

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,13 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: 'assets/[name]-[hash].js',
+        chunkFileNames: 'assets/[name]-[hash].js',
+      }
+    }
+  }
 });
